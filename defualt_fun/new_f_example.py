4218f66e5e2e47fb3dfa7bbdf110cf53a6badb32
Manmanth Mengji <mengjimanmanth@gmail.com>
2025-03-02 13:18:21 +0530

    Added a new file new_file.py with basic while loop demo and made changes to range.py just a new line of message, Nothing else!

---
+++ defualt_fun/new_f_example.py
@@ -0,0 +1,6 @@
+print("this is an updated code!!")
+i = 1
+while i < 10:
+    print(f'value of i is {i}')
+    i += 1
+print(f"{i} is no longer greater then 10")
--- defualt_fun/range_opt.py
+++ defualt_fun/range_opt.py
@@ -12,6 +12,7 @@ x = 10
 for i in range(x):
     print(f'Value of x is {i}')
 print("Execution Complete!")
+print("Another line of code")
 
 
 
