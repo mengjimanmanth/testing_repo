2b2bd446e6968cde85750edc396e5d37f817b9ec
Manmanth Mengji <mengjimanmanth@gmail.com>
2025-03-02 12:33:49 +0530

    Use of range function and used format in print statement

---
+++ defualt_fun/range_opt.py
@@ -0,0 +1,16 @@
+"""
+Range Function:
+    -> The range function is used to generate a sequence of numbers, mainly in loops.
+    -> syntax,
+        range(start, stop, step)
+        -> Start, (Optional) The starting number (defualt is 0).
+        -> Stop, (Required) The number where it stops (Not Included).
+        -> Step, (Optional) The increment/decrement (defualt is 1)
+"""
+print("check")
+x = 10
+for i in range(x):
+    print(f'Value of x is {i}')
+
+
+
